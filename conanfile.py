3ee8c5fa2c8d886e29e35f98bfe8746e4a5914a8
ross <rossbennett34@gmail.com>
2022-03-30 05:57:54 -0500

    move free functions to anonymous namespace

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conans import ConanFile, CMake, tools
+
+
+class NetConan(ConanFile):
+    name = "wildcat-ws"
+    version = "0.1.1"
+    license = "MIT"
+    author = "<Ross Bennett> <rossbennett34@gmail.com>"
+    url = "https://github.com/rossb34/wildcat-ws"
+    description = "Web socket library"
+    exports_sources = "include/*"
+    no_copy_source = True
+
+    def package(self):
+        self.copy("*.hpp")
+
+    def package_id(self):
+        self.info.header_only()
